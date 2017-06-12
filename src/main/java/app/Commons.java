c41a65184547f675a3bad146e0a1429018aad6aa
Kjetil Kjørstad <kjorstad@live.no>
2017-06-12 23:15:02 +0200

    First commit

---
+++ src/main/java/app/Commons.java
@@ -0,0 +1,15 @@
+package app;
+
+public interface Commons {
+    
+    public static final int WIDTH = 400;
+    public static final int HEIGTH = 500;
+    public static final int BOTTOM_EDGE = HEIGTH - 10;
+    public static final int N_OF_BRICKS = 30;
+    public static final int INIT_PADDLE_X = (WIDTH / 2) - 20;
+    public static final int INIT_PADDLE_Y = HEIGTH - 50;
+    public static final int INIT_BALL_X = 230;
+    public static final int INIT_BALL_Y = 355;    
+    public static final int DELAY = 1000;
+    public static final int PERIOD = 10;
+}
